0fc84c0c85d3e018959d23cd91a074261049da30
Admin <maayush235@gmail.com>
2024-05-25 21:08:37 +0530

    Radhey Radhey

--- lib/Providers/All_Providers.dart
+++ lib/Providers/All_Providers.dart
@@ -69,16 +69,16 @@ class MainProvider extends ChangeNotifier
   void updateSearchEngine(String value) {
     selectedSearchEngine = value;
     if (selectedSearchEngine == 'Google') {
-      inAppWebViewController!.loadUrl(
+      inAppWebViewController.loadUrl(
           urlRequest: URLRequest(url: WebUri("https://www.google.com/")));
     } else if (selectedSearchEngine == 'Yahoo') {
-      inAppWebViewController!.loadUrl(
+      inAppWebViewController.loadUrl(
           urlRequest: URLRequest(url: WebUri("https://in.search.yahoo.com/")));
     } else if (selectedSearchEngine == 'Bing') {
-      inAppWebViewController!.loadUrl(
+      inAppWebViewController.loadUrl(
           urlRequest: URLRequest(url: WebUri("https://www.bing.com/")));
     } else if (selectedSearchEngine == 'Duck Duck Go') {
-      inAppWebViewController!.loadUrl(
+      inAppWebViewController.loadUrl(
           urlRequest: URLRequest(url: WebUri("https://duckduckgo.com/")));
     }
     notifyListeners();
@@ -92,22 +92,22 @@ class MainProvider extends ChangeNotifier
   void searchEngines()
   {
     if (selectedSearchEngine == 'Google') {
-      inAppWebViewController!.loadUrl(
+      inAppWebViewController.loadUrl(
           urlRequest: URLRequest(
               url: WebUri(
                   "https://www.google.com/search?q=${newsearchtext}&rlz=1C1ONGR_enIN1095IN1095&oq=hi&gs_lcrp=EgZjaHJvbWUqDggAEEUYJxg7GIAEGIoFMg4IABBFGCcYOxiABBiKBTIGCAEQRRg7Mg0IAhAAGIMBGLEDGIAEMg0IAxAuGIMBGLEDGIAEMhAIBBAAGIMBGLEDGIAEGIoFMg0IBRAAGIMBGLEDGIAEMhYIBhAuGIMBGMcBGLEDGNEDGIAEGIoFMgcIBxAAGIAEMgcICBAAGIAEMhAICRAuGIMBGLEDGIAEGIoF0gEJMTIzNmowajE1qAIJsAIB&sourceid=chrome&ie=UTF-8")));
     } else if (selectedSearchEngine == 'Yahoo') {
-      inAppWebViewController!.loadUrl(
+      inAppWebViewController.loadUrl(
           urlRequest: URLRequest(
               url: WebUri(
                   "https://in.search.yahoo.com/search;_ylt=AwrPoYNX0lFm3wcpV0y7HAx.;_ylc=X1MDMjExNDcyMzAwMwRfcgMyBGZyA2NybWFzBGZyMgNzYi10b3AEZ3ByaWQDZlRUNGtwSXdUSjZmZURhT2FPdnRXQQRuX3JzbHQDMARuX3N1Z2cDMTAEb3JpZ2luA2luLnNlYXJjaC55YWhvby5jb20EcG9zAzAEcHFzdHIDBHBxc3RybAMwBHFzdHJsAzExBHF1ZXJ5A2ZsdXR0ZXIlMjBkZXYEdF9zdG1wAzE3MTY2MzgzMDE-?p=${newsearchtext }&fr2=sb-top&fr=crmas&vm=r")));
     } else if (selectedSearchEngine == 'Bing') {
-      inAppWebViewController!.loadUrl(
+      inAppWebViewController.loadUrl(
           urlRequest: URLRequest(
               url: WebUri(
                   "https://www.bing.com/search?q=${newsearchtext}&qs=n&form=QBRE&sp=-1&lq=0&pq=flutter+dev&sc=19-11&sk=&cvid=B4B782306ED143AE9326650EBDDC17DC&ghsh=0&ghacc=0&ghpl=")));
     } else if (selectedSearchEngine == 'Duck Duck Go') {
-      inAppWebViewController!.loadUrl(
+      inAppWebViewController.loadUrl(
           urlRequest: URLRequest(
               url: WebUri("https://duckduckgo.com/?q=${newsearchtext}&ia=web")));
     }
