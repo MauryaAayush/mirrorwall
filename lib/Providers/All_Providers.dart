8557b8733dbb185f2eeff2d0c89647dde32ecb00
Admin <maayush235@gmail.com>
2024-05-25 21:14:22 +0530

    Radhey Radhey

--- lib/Providers/All_Providers.dart
+++ lib/Providers/All_Providers.dart
@@ -4,12 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:untitled/Utils/Global_Variables.dart';
 
-class MainProvider extends ChangeNotifier
-{
+class MainProvider extends ChangeNotifier {
   double progress = 0;
   String searchtext = 'Google';
   String newsearchtext = '';
-
 
   String groupValue = 'Google';
   List searchEngineNames = ['Google', 'Bing', 'Duck Duck Go', 'Yahoo'];
@@ -17,20 +15,19 @@ class MainProvider extends ChangeNotifier
   String? searchedUrl;
   bool isButtonEnabled = true;
 
-  List <String> bookmarkList= [];
+  List<String> bookmarkList = [];
   String? currentUrl;
-  List <String> historylist = [];
+  List<String> historylist = [];
   String? searchvalue;
 
-  void onchange_progress(int progress)
-  {
-    this.progress = progress/100;
+  Future<void> onchange_progress(int progress) async {
+    this.progress = progress / 100;
     notifyListeners();
   }
 
   Future<bool> timePass() async {
     if (isButtonEnabled) {
-      await inAppWebViewController!.goBack();
+      await inAppWebViewController.goBack();
       return true;
     }
 
@@ -38,29 +35,24 @@ class MainProvider extends ChangeNotifier
     return false;
   }
 
-  void search(String searchtext)
-  {
+  void search(String searchtext) {
     this.searchtext = searchtext;
     notifyListeners();
   }
 
-  Future<void> setcurrentUrl()
-  async {
+  Future<void> setcurrentUrl() async {
     WebUri? webUri = await inAppWebViewController.getUrl();
     currentUrl = await inAppWebViewController.getTitle() as String;
     notifyListeners();
   }
 
-  void addtoBookMark()
-  {
+  void addtoBookMark() {
     log('link asd----------------------------------------------');
     bookmarkList.add(currentUrl!);
     notifyListeners();
   }
 
-
-  void addtoHistory()
-  {
+  void addtoHistory() {
     historylist.add(currentUrl!);
     log('link added------------------------------------------------');
     notifyListeners();
@@ -89,27 +81,26 @@ class MainProvider extends ChangeNotifier
     notifyListeners();
   }
 
-  void searchEngines()
-  {
+  void searchEngines() {
     if (selectedSearchEngine == 'Google') {
       inAppWebViewController.loadUrl(
           urlRequest: URLRequest(
               url: WebUri(
-                  "https://www.google.com/search?q=${newsearchtext}&rlz=1C1ONGR_enIN1095IN1095&oq=hi&gs_lcrp=EgZjaHJvbWUqDggAEEUYJxg7GIAEGIoFMg4IABBFGCcYOxiABBiKBTIGCAEQRRg7Mg0IAhAAGIMBGLEDGIAEMg0IAxAuGIMBGLEDGIAEMhAIBBAAGIMBGLEDGIAEGIoFMg0IBRAAGIMBGLEDGIAEMhYIBhAuGIMBGMcBGLEDGNEDGIAEGIoFMgcIBxAAGIAEMgcICBAAGIAEMhAICRAuGIMBGLEDGIAEGIoF0gEJMTIzNmowajE1qAIJsAIB&sourceid=chrome&ie=UTF-8")));
+                  "https://www.google.com/search?q=$newsearchtext&rlz=1C1ONGR_enIN1095IN1095&oq=hi&gs_lcrp=EgZjaHJvbWUqDggAEEUYJxg7GIAEGIoFMg4IABBFGCcYOxiABBiKBTIGCAEQRRg7Mg0IAhAAGIMBGLEDGIAEMg0IAxAuGIMBGLEDGIAEMhAIBBAAGIMBGLEDGIAEGIoFMg0IBRAAGIMBGLEDGIAEMhYIBhAuGIMBGMcBGLEDGNEDGIAEGIoFMgcIBxAAGIAEMgcICBAAGIAEMhAICRAuGIMBGLEDGIAEGIoF0gEJMTIzNmowajE1qAIJsAIB&sourceid=chrome&ie=UTF-8")));
     } else if (selectedSearchEngine == 'Yahoo') {
       inAppWebViewController.loadUrl(
           urlRequest: URLRequest(
               url: WebUri(
-                  "https://in.search.yahoo.com/search;_ylt=AwrPoYNX0lFm3wcpV0y7HAx.;_ylc=X1MDMjExNDcyMzAwMwRfcgMyBGZyA2NybWFzBGZyMgNzYi10b3AEZ3ByaWQDZlRUNGtwSXdUSjZmZURhT2FPdnRXQQRuX3JzbHQDMARuX3N1Z2cDMTAEb3JpZ2luA2luLnNlYXJjaC55YWhvby5jb20EcG9zAzAEcHFzdHIDBHBxc3RybAMwBHFzdHJsAzExBHF1ZXJ5A2ZsdXR0ZXIlMjBkZXYEdF9zdG1wAzE3MTY2MzgzMDE-?p=${newsearchtext }&fr2=sb-top&fr=crmas&vm=r")));
+                  "https://in.search.yahoo.com/search;_ylt=AwrPoYNX0lFm3wcpV0y7HAx.;_ylc=X1MDMjExNDcyMzAwMwRfcgMyBGZyA2NybWFzBGZyMgNzYi10b3AEZ3ByaWQDZlRUNGtwSXdUSjZmZURhT2FPdnRXQQRuX3JzbHQDMARuX3N1Z2cDMTAEb3JpZ2luA2luLnNlYXJjaC55YWhvby5jb20EcG9zAzAEcHFzdHIDBHBxc3RybAMwBHFzdHJsAzExBHF1ZXJ5A2ZsdXR0ZXIlMjBkZXYEdF9zdG1wAzE3MTY2MzgzMDE-?p=$newsearchtext &fr2=sb-top&fr=crmas&vm=r")));
     } else if (selectedSearchEngine == 'Bing') {
       inAppWebViewController.loadUrl(
           urlRequest: URLRequest(
               url: WebUri(
-                  "https://www.bing.com/search?q=${newsearchtext}&qs=n&form=QBRE&sp=-1&lq=0&pq=flutter+dev&sc=19-11&sk=&cvid=B4B782306ED143AE9326650EBDDC17DC&ghsh=0&ghacc=0&ghpl=")));
+                  "https://www.bing.com/search?q=$newsearchtext&qs=n&form=QBRE&sp=-1&lq=0&pq=flutter+dev&sc=19-11&sk=&cvid=B4B782306ED143AE9326650EBDDC17DC&ghsh=0&ghacc=0&ghpl=")));
     } else if (selectedSearchEngine == 'Duck Duck Go') {
       inAppWebViewController.loadUrl(
           urlRequest: URLRequest(
-              url: WebUri("https://duckduckgo.com/?q=${newsearchtext}&ia=web")));
+              url: WebUri("https://duckduckgo.com/?q=$newsearchtext&ia=web")));
     }
   }
 
@@ -118,7 +109,7 @@ class MainProvider extends ChangeNotifier
         searchedUrl == 'https://in.search.yahoo.com/' ||
         searchedUrl == 'https://www.bing.com/' ||
         searchedUrl == 'https://duckduckgo.com/') {
-      print(searchedUrl);
+      // print(searchedUrl);
       isButtonEnabled = false;
       notifyListeners();
     } else {
@@ -127,10 +118,8 @@ class MainProvider extends ChangeNotifier
     }
   }
 
-
   void updateSearchedText(String text) {
     newsearchtext = text;
     notifyListeners();
   }
-
 }
