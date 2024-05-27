a50610bfed1b293c52a50b79484960c28e13194c
Admin <maayush235@gmail.com>
2024-05-27 12:46:23 +0530

    Radhey Radhey

--- lib/Views/Main_Screen/Main_Screen.dart
+++ lib/Views/Main_Screen/Main_Screen.dart
@@ -30,7 +30,6 @@ class MainScreen extends StatelessWidget {
             width: 350,
             child: Header_TextFormField(),
           ),
-
           // this is pop up menu of trailing
           PopUpofAppBar(),
         ],
